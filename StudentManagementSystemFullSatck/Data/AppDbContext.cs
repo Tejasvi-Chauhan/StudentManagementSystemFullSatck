909faf1f5fa151f2fc15b11f9978c91dacb49fce
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-03-09 16:16:38 +0530

    Add project files.

---
+++ StudentManagementSystemFullSatck/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementSystemFullSatck.Models;
+
+
+namespace StudentManagementSystemFullSatck.Data
+{
+    public class AppDbContext
+    {
+    }
+}
