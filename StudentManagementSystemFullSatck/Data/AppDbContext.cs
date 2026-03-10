3cf528f69c3fa581b8943a5d12aec029275ff41d
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-03-10 10:02:42 +0530

    Day 10: Install EF Core setup Db context class and models

--- StudentManagementSystemFullSatck/Data/AppDbContext.cs
+++ StudentManagementSystemFullSatck/Data/AppDbContext.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using StudentManagementSystemFullSatck.Models;
+using StudentManagementSystemFullStack.Models;
 
 
-namespace StudentManagementSystemFullSatck.Data
+namespace StudentManagementSystemFullStack.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        //public DbSet<Student> Students { get; set; } 
+        //public DbSet<Teacher> Teachers { get; set; }
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<StudentCourse> StudentCourses { get; set; }
+        //public DbSet<StudentTeacher> StudentTeachers { get; set; }
+        //public DbSet<Mark> Marks { get; set; }
+        //public DbSet<ProfileUpdateRequest> ProfileUpdateRequests { get; set; }  
+       
+
+
     }
 }
